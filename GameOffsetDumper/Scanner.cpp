0c0d23b0e49c5c72a9999b286f74dbb5d0c1c6f1
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2020-09-20 01:06:04 +0800

    Fixed a bug :bug:
    Almost completed a FindPattern function.
    It does find whether the pattern exsists or not, but still doesn't know where is that

--- GameOffsetDumper/Scanner.cpp
+++ GameOffsetDumper/Scanner.cpp
@@ -5,16 +5,20 @@
 void Scanner::FindPattern(std::vector<int>& signature, Process& process)
 {
     std::vector<std::byte> buffer(process.moduleBaseSize);
-    std::cout << signature.size() << std::endl;
     ReadProcessMemory(process.hProcess, static_cast<LPCVOID>(process.moduleBaseAddress), buffer.data(), buffer.size(), NULL);
+    int iStore = 0;
     for (int i = 0;i < buffer.size();++i) {
         for (int j = 0;j < signature.size();++j) {
+            iStore = i;
             if (signature.at(j) == -1 || static_cast<std::byte>(signature.at(j)) == buffer.at(i)) {
-                if (j == signature.size()) {
+                if (j + 1 == signature.size()) {
                     std::cout << "FOUND" << std::endl;
+                    return;
                 }
-                continue;
-            } else { //static_cast<std::byte>(signature.at(j)) != buffer.at(i)
+                //std::cout << signature.at(j) << " " << static_cast<int>(buffer.at(i)) << std::endl;
+                i++;
+            } else {
+                i = iStore;
                 break;
             }
         }
