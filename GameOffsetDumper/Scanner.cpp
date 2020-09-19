63963b359a0e0e61e567ee1b10ca961b116bec44
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2020-09-20 03:26:44 +0800

    I fixed a bug :bug:
    Changed a condition of buffer iteration to prevent "accessing out of range" error from occurring.

--- GameOffsetDumper/Scanner.cpp
+++ GameOffsetDumper/Scanner.cpp
@@ -7,7 +7,7 @@ std::optional<uintptr_t> Scanner::FindPattern(std::vector<int>& signature, Proce
     std::vector<std::byte> buffer(process.moduleBaseSize);
     ReadProcessMemory(process.hProcess, static_cast<LPCVOID>(process.moduleBaseAddress), buffer.data(), buffer.size(), NULL);
     int iStore = 0;
-    for (int i = 0;i < buffer.size();++i) {
+    for (int i = 0;i < buffer.size() - signature.size() + 1;++i) {
         for (int j = 0;j < signature.size();++j) {
             iStore = i;
             if (signature.at(j) == -1 || static_cast<std::byte>(signature.at(j)) == buffer.at(i)) {
