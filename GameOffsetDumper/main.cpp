985cb88a4a635a2508122675ce67141a941e7f01
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2020-09-24 17:29:21 +0800

    Console will left after program terminated
    I added AllocConsole and Terminate function to make console appear and left after either program will finished or hang up

--- GameOffsetDumper/main.cpp
+++ GameOffsetDumper/main.cpp
@@ -7,12 +7,26 @@
 #include "Scanner.h"
 #include "Exporter.h"
 
+void Terminate() {
+    std::cout << "\n\nPress any key to finish..." << std::endl;
+    std::cin.get();
+    fclose(stdout);
+    fclose(stderr);
+    fclose(stdin);
+    FreeConsole();
+}
 int main()
 {
+    AllocConsole();
+    FILE* fp = NULL;
+    freopen_s(&fp, "CONOUT$", "w", stdout);
+    freopen_s(&fp, "CONOUT$", "w", stderr);
+    freopen_s(&fp, "CONIN$", "r", stdin);
     std::string configFileName = "config.toml";
     std::optional<std::string> gameName = ConfigReader::ReadGameName(configFileName);
     if (!gameName) {
         std::cerr << "Something went wrong. Make sure if you have config.toml and specified the name of a game" << std::endl;
+        Terminate();
         return 1;
     }
     std::string dir = ConfigReader::ReadExportDir(configFileName);
@@ -24,18 +38,23 @@ int main()
         Process prc = Process::GetProcess(*gameName, config.module);
         if (auto e = prc.GetError(); e) {
             std::cerr << *e;
+            Terminate();
             return 1;
         }
         auto result = Scanner::Scan(config.signature, prc, config);
         if (!result) {
             std::cerr << "Couldn't find signature. Check if you filled config.toml properly." << std::endl;
+            Terminate();
             return 1;
         }
-        //std::cout << "<" << config.module << ">" << " + 0x" << std::hex << *result << std::endl;
+        std::cout << "<" << config.module << ">" << " + 0x" << std::hex << *result << std::endl;
         offsetInfo.emplace_back(std::make_tuple(config.name, *result, config.module));
     }
 
     std::string exptDir = dir + "GameOffsets.hpp";
     Exporter exp(exptDir, offsetInfo);
     exp.WriteDown();
+    std::cout << "\nGameOffsetDumper.exe has successfully finished its work:)" << std::endl;
+    Terminate();
+    return 0;
 }
