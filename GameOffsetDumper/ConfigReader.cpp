46b76c0144db4465840752c10f99978f834f5c59
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2020-09-24 18:57:01 +0800

    Replace word "targets" with "profile" in config

--- GameOffsetDumper/ConfigReader.cpp
+++ GameOffsetDumper/ConfigReader.cpp
@@ -7,8 +7,8 @@ std::vector<SignatureInfo> ConfigReader::ReadSigs(const std::string& filename)
 {
     auto config = toml::parse(filename);
     std::vector<SignatureInfo> result = {};
-    for (auto& target : config.at("targets").as_array()) {
-        SignatureInfo si(target.at("name").as_string(), target.at("signature").as_string(), target.at("module").as_string(), target.at("offset").as_integer(), target.at("extra").as_integer());
+    for (auto& profile : config.at("profile").as_array()) {
+        SignatureInfo si(profile.at("name").as_string(), profile.at("signature").as_string(), profile.at("module").as_string(), profile.at("offset").as_integer(), profile.at("extra").as_integer());
         result.push_back(si);
     }
     return result;
